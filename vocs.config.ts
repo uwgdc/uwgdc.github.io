4c7b97a73014d3b60ec4d43abb9d2a079de5b938
Avafe <65048459+ImAvafe@users.noreply.github.com>
2026-02-10 20:17:41 -0500

    Config site, add welcome content, etc

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,15 +1,60 @@
-import { defineConfig } from 'vocs'
+import { defineConfig } from "vocs";
 
 export default defineConfig({
-  title: 'Docs',
-  sidebar: [
-    {
-      text: 'Getting Started',
-      link: '/getting-started',
-    },
-    {
-      text: 'Example',
-      link: '/example',
-    },
-  ],
-})
+	title: "UW Gamedev",
+	description: "Game development resources from the University of Waterloo.",
+	aiCta: false,
+	sidebar: [
+		{
+			text: "PRESS START",
+			items: [
+				{
+					text: "Welcome!",
+					link: "/intro",
+				},
+				{
+					text: "Choosing an Engine",
+					link: "/engines",
+				},
+			],
+		},
+	],
+	socials: [
+		{
+			icon: "github",
+			link: "https://github.com/uwgdc",
+		},
+		{
+			icon: "discord",
+			link: "https://discord.gg/tHEXh9d",
+		},
+	],
+	logoUrl: {
+		light: "/logo-light.svg",
+		dark: "/logo-dark.svg",
+	},
+	iconUrl: {
+		light: "/icon-light.svg",
+		dark: "/icon-dark.svg",
+	},
+	ogImageUrl: "/og-image.png",
+	font: {
+		google: "Rubik",
+	},
+	theme: {
+		accentColor: {
+			light: "#F7B543",
+			dark: "#F6CF51",
+		},
+		variables: {
+			fontSize: {
+				root: "16",
+			},
+			fontWeight: {
+				regular: "400",
+				medium: "450",
+				semibold: "500",
+			},
+		},
+	},
+});
