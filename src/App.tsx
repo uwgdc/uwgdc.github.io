c94d13ac87b493e8ca9e309ab075aa118d48923d
Avafe <65048459+ImAvafe@users.noreply.github.com>
2026-03-04 16:34:44 -0500

    Install TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import "./index.css";
+
 export default function Main() {
-	return <h1>hello world</h1>;
+	return (
+		<h1 className="text-3xl font-semibold font-mono">
+			vite + react + tailwind :D
+		</h1>
+	);
 }
