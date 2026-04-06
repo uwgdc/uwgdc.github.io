7eaf8418236be1ed82c6ba3275c08d4492e57839
Avafe <65048459+ImAvafe@users.noreply.github.com>
2026-04-06 15:02:45 -0400

    Populate games carousel

--- src/data/games.ts
+++ src/data/games.ts
@@ -9,16 +9,20 @@ const GAMES: Game[] = [
   {
     name: "Grey Wake",
     creator: "Second Fiddle Studio",
-    banner:
-      "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/3631930/3e91aeadeec378bdd956fe0ec618d5660f3fbad5/header.jpg?t=1770680947",
+    banner: "src/assets/games/GreyWake.webp",
     link: "https://store.steampowered.com/app/3631930/Grey_Wake/",
   },
   {
-    name: "Gay Woke",
-    creator: "Second Fiddle Studio",
-    banner:
-      "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/3631930/dc3ef44922f136f61f8552ef36c928685ed513ba/ss_dc3ef44922f136f61f8552ef36c928685ed513ba.1920x1080.jpg?t=1770680947",
-    link: "https://store.steampowered.com/app/3631930/Grey_Wake/",
+    name: "Khaos Keys",
+    creator: "qwqsoft",
+    banner: "src/assets/games/KhaosKeys.webp",
+    link: "https://youtu.be/mieGHKDegrQ",
+  },
+  {
+    name: "Seas of Static",
+    creator: "Fishbed Studios",
+    banner: "src/assets/games/SeasOfStatic.webp",
+    link: "https://www.youtube.com/watch?v=LsFUsDV-cr4",
   },
 ];
 
